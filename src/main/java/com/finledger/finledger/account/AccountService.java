c5e083e98d23970ee1e6409c0a03fc7ecd2032ae
Hyeok8598 <soonh8598@gmail.com>
2026-03-08 02:16:54 +0900

    feat009: 계좌 이체 기능 구현 - AccountService 계좌 이체 기능 추가 - Pessimistic Lock 기반 계좌 조회 추가 - 계좌 이체 테스트 작성

--- src/main/java/com/finledger/finledger/account/AccountService.java
+++ src/main/java/com/finledger/finledger/account/AccountService.java
@@ -1,6 +1,6 @@
 package com.finledger.finledger.account;
 
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -54,6 +54,24 @@ public class AccountService {
         return this.getAccountByAccountNumber(accountNumber).getBalance();
     }
 
+    @Transactional
+    public void transfer(Long fromId, Long toId, BigDecimal amount) {
+        validateTransferRequest(fromId, toId, amount);
+
+        /* Dead Lock 방지 */
+        Long firstID  = Math.min(fromId, toId);
+        Long secondID = Math.max(fromId, toId);
+
+        Account firstAccount  = getAccountForUpdate(firstID);
+        Account secondAccount = getAccountForUpdate(secondID);
+
+        Account fromAccount = firstAccount.getId().equals(fromId) ? firstAccount : secondAccount;
+        Account toAccount   = firstAccount.getId().equals(toId) ? firstAccount : secondAccount;
+
+        fromAccount.withdraw(amount);
+        toAccount.deposit(amount);
+    }
+
     private Account getAccount(Long id) {
         return accountRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("계좌가 존재하지 않습니다."));
@@ -62,5 +80,24 @@ public class AccountService {
     private Account getAccountByAccountNumber(String accountNumber) {
         return accountRepository.findByAccountNumber(accountNumber)
                 .orElseThrow(() -> new IllegalArgumentException("계좌가 존재하지 않습니다."));
+    }
+
+    private Account getAccountForUpdate(Long id) {
+        return accountRepository.findByIdForUpdate(id)
+                .orElseThrow(() -> new IllegalArgumentException("계좌가 존재하지 않습니다."));
+    }
+
+    private void validateTransferRequest(Long fromId, Long toId, BigDecimal amount) {
+        if(fromId == null || toId == null) {
+            throw new IllegalArgumentException("계좌가 존재하지 않습니다.");
+        }
+
+        if(fromId.equals(toId)) {
+            throw new IllegalArgumentException("동일 계좌로 이체할 수 없습니다.");
+        }
+
+        if(amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("이체 금액은 0보다 커야 합니다.");
+        }
     }
 }
