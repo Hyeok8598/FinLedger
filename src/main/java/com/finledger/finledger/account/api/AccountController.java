1eaaa79e0174f38e36048cff045bf16581aa1e6c
Hyeok8598 <soonh8598@gmail.com>
2026-03-28 12:57:05 +0900

    feature015: 공통 API 응답 구조 적용 - ApiResponse<T> 기반 성공/실패 응답 구조 통일 - 전역 예외 처리 적용

--- src/main/java/com/finledger/finledger/account/api/AccountController.java
+++ src/main/java/com/finledger/finledger/account/api/AccountController.java
@@ -1,8 +1,10 @@
 package com.finledger.finledger.account.api;
 
+import com.finledger.finledger.account.AccountRepository;
 import com.finledger.finledger.account.AccountService;
 import com.finledger.finledger.account.api.dto.AmountRequest;
 import com.finledger.finledger.account.api.dto.CreateAccountRequest;
+import com.finledger.finledger.common.api.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,27 +18,32 @@ public class AccountController {
     private final AccountService accountService;
 
     @PostMapping
-    public Long create(@RequestBody CreateAccountRequest request) {
-        return accountService.createAccount(
-                request.getOwnerName(),
-                request.getInitialBalance()
+    public ApiResponse<Long> create(@RequestBody CreateAccountRequest request) {
+        return ApiResponse.success(
+          accountService.createAccount(
+                  request.getOwnerName(),
+                  request.getInitialBalance())
         );
     }
 
     @GetMapping("/{id}/balance")
-    public BigDecimal balance(@PathVariable Long id) {
-        return accountService.getBalance(id);
+    public ApiResponse<BigDecimal> balance(@PathVariable Long id) {
+        return ApiResponse.success(
+                accountService.getBalance(id)
+        );
     }
 
     @PostMapping("/{id}/deposit")
-    public void deposit(@PathVariable Long id,
+    public ApiResponse<Void> deposit(@PathVariable Long id,
                         @RequestBody AmountRequest request) {
         accountService.deposit(id, request.getAmount());
+        return ApiResponse.success();
     }
 
     @PostMapping("/{id}/withdraw")
-    public void withdraw(@PathVariable Long id,
+    public ApiResponse<Void> withdraw(@PathVariable Long id,
                          @RequestBody AmountRequest request) {
         accountService.withdraw(id, request.getAmount());
+        return ApiResponse.success();
     }
 }
--- src/main/java/com/finledger/finledger/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/finledger/finledger/common/exception/GlobalExceptionHandler.java
@@ -1,30 +1,25 @@
 package com.finledger.finledger.common.exception;
 
+import com.finledger.finledger.common.api.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestController
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
-        ErrorResponse response = new ErrorResponse(
-                e.getErrorCode().name(),
-                e.getMessage()
-        );
-
-        return ResponseEntity.badRequest().body(response);
+    public ResponseEntity<ApiResponse<Void>> handleBusinessException(BusinessException e) {
+        return ResponseEntity.badRequest()
+                .body(ApiResponse.fail(e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleUnexpectedExcecption(Exception e) {
-        ErrorResponse response = new ErrorResponse(
-                "INTERNAL_SERVER_ERROR",
-                "알수없는 오류가 발생했습니다."
-        );
-
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    public ResponseEntity<ApiResponse<Void>> handleUnexpectedException(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.fail("알 수 없는 오류가 발생했습니다."));
     }
 }
